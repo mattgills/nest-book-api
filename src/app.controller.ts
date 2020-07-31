8c6358b579082131bfbfeb11f6d78974c76bf576
mattgills <matt.gillespie24@gmail.com>
2020-07-31 14:36:23 -0400

    Removes proflie route since it may not be implemented for other apis

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -18,9 +18,9 @@ export class AppController {
     return this.authService.login(req.user);
   }
 
-  @UseGuards(JwtAuthGuard)
-  @Get('profile')
-  getProfile(@Request() req) {
-    return req.user;
-  }
+  // @UseGuards(JwtAuthGuard)
+  // @Get('profile')
+  // getProfile(@Request() req) {
+  //   return req.user;
+  // }
 }
